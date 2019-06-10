3df96e19095541c14116a7fbbe4fc7dcfb16486c
sgnls <sgnls@users.noreply.github.com>
2019-06-10 22:45:59 +0100

    Delete phr-o365.ps1

--- phr-o365.ps1
+++
@@ -1,3 +0,0 @@
-(get-mailbox).alias | % { "# $_ "; get-inboxrule -mailbox $_ | ? {$_.forwardto -ne $null} | fl forwardto* }
-(get-mailbox).alias | % { "# $_ "; get-inboxrule -mailbox $_ | ? {$_.description -match "If the"} | fl description}
-$a = "prettylotus@vip.163.com" ;"get-accepteddomain | ? {$_.name -notmatch "microsoft"} | % { get-messagetrace -pagesize 5000 | ? {$_.senderaddress -match "$a"} | ft received,sender*,recipient*,subject*,status* }
